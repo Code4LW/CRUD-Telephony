33e9877d600b25ee8a4999ae61b9507bdb66783a
Zanggar <zangar992103@gmail.com>
2023-10-29 00:27:32 +0600

    Проект создан

---
+++ src/main/java/com/example/cgmzswnvfqvoqrnovnla/CgmzswnvfqvoqrnovnlaApplication.java
@@ -0,0 +1,13 @@
+package com.example.cgmzswnvfqvoqrnovnla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgmzswnvfqvoqrnovnlaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CgmzswnvfqvoqrnovnlaApplication.class, args);
+	}
+
+}
